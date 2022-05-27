c6fc09796b26726db472ca95f5baf5bd5f01a3d7
hugojenkins11 <hjenkins30@stuy.edu>
2022-05-26 20:33:43 -0400

    ok

---
+++ v0/Woo.java
@@ -0,0 +1,11 @@
+/*
+AKH+D: Hugo Jenkins, Ariella Katz, Kaitlin Ho, Boary, Tom, Apple
+APCS pd6
+FP -- Are You a Player?
+2022-05-26
+*/
+public class Woo {
+  public static void main(String[] args) {
+    System.out.println("This is our v0 beep bop");
+  }
+}
