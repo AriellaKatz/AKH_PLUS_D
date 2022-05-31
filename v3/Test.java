29b36e7c478fe408aafb36c772fa1294ac8050f2
Hugo Jenkins <hjenkins30@stuy.edu>
2022-05-31 12:28:06 -0400

    Hugo's Genius

---
+++ v3/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+  public static void main(String[] args) {
+    Scanny y = new Scanny();
+    String s = y.toString();
+    if (s.equals("1")) System.out.println(true);
+    if (s.equals("2")) System.out.println(false);
+
+  }
+}
