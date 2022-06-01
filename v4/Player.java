6926993c926aeaeef451ec5e686efab56df49d51
AriellaKatz <akatz30@stuy.edu>
2022-06-01 12:12:44 -0400

    add v4, trying something different

---
+++ v4/Player.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Player{
+
+private ArrayList<Character> _rank;
+
+//sorts the characters based on how much they like the protagonist (likeabliity)
+// public void sortRank(){
+//
+// }
+
+}
---
+++ v4/Woo.java
@@ -0,0 +1,35 @@
+/*
+AKH+D: Hugo Jenkins, Ariella Katz, Kaitlin Ho, Boary, Tom, Apple
+APCS pd6
+FP -- Are You a Player?
+2022-05-26
+*/
+
+import java.util.ArrayList;
+
+public class Woo {
+  public static void main(String[] args) {
+    System.out.println("This is our v0 beep bop");
+  }
+
+  //private Brad _brad;
+  private Jessica _jessica;
+  //private Richard _richard;
+  private Player _player;
+  private boolean _gameOver;
+
+  public Woo(){
+// characters will be instaniated after get to know you method (they start out)
+// with certain attraction levels
+  }
+
+  public void play(){
+
+  }
+
+  // public String probeTree(Character character){
+  //
+  // }
+
+
+}
